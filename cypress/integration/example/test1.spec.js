106944a1d00dcdcd42a8a238ccea9cef1f1bcb2d
Joseph Loutfi <joseph@anghami.com>
2021-11-07 17:30:12 +0400

    On testrunner, headed and headless modes and running the tests in different browsers, commands and scripts run from npm and a general intro to the framework structure

---
+++ cypress/integration/example/test1.spec.js
@@ -0,0 +1,9 @@
+describe('My first test suite', function (){
+    it('My 1st test case', function (){
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+    })
+
+    it('My 2nd test case', function () {
+        
+    })
+})
