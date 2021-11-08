1c03a2dd66e44d75003aed4692fc9a29e7f03889
Joseph Loutfi <joseph@anghami.com>
2021-11-08 15:31:52 +0400

    JS basic: arrays: sort, reverse, map, reduce, filter

--- js_basics/basics3.js
+++ js_basics/basics3.js
@@ -41,3 +41,51 @@ for (let i = 0; i < marks3.length; i++){
 }
 
 console.log(sum)
+
+//**********************
+//reduce, filter and map
+console.log('\n*************************\n')
+
+console.log(marks3)
+marks3.reduce(getSum, 1000)
+
+function getSum(total, currentValue) {
+    console.log(currentValue)
+    console.log(total)
+    return total + currentValue
+}
+
+console.log(marks3.reduce((total, num) => total + num, 0))
+
+
+var scores = [12, 13, 14, 15]
+
+let newFilteredArray = scores.filter(score  => score % 2 == 0)
+console.log(newFilteredArray)
+
+let newMappedArray = newFilteredArray.map(score => score * 3)
+console.log(newMappedArray)
+
+let scores2 = [12, 13, 14, 15]
+
+let result = scores2.filter(score => score % 2 == 0).map(score => score * 3).reduce((sum, val) => sum + val, 0)
+console.log(result)
+
+// sort
+
+let fruits = ['apple', 'mango', 'banana', 'lemon', 'Mango']
+console.log(fruits.sort())
+
+let numbers = [4,5,2,9,0,3]
+console.log(numbers.sort())
+
+var numbers2 = [4,5,2,003,9,0]
+console.log(numbers2.sort())
+
+
+var numbers2 = [4,5,2,003,9,0]
+console.log(numbers2.sort((a,b) => a-b))
+console.log(numbers2.sort((a,b) => b-a))
+
+var numbers2 = [4,5,2,003,9,0]
+console.log(numbers2.reverse())
