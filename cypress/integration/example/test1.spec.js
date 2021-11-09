f5f60715b384183d7394d795e405c7e20a96737a
Joseph Loutfi <joseph@anghami.com>
2021-11-09 17:36:38 +0400

    cypress locator strategies: how to locate elements on the page

--- cypress/integration/example/test1.spec.js
+++ cypress/integration/example/test1.spec.js
@@ -1,9 +1,12 @@
 describe('My first test suite', function (){
     it('My 1st test case', function (){
         cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
-    })
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+        cy.get('.product:visible').should('have.length', 4);
 
-    it('My 2nd test case', function () {
-        
+
+
+
     })
 })
