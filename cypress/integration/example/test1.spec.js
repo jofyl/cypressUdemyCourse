6b7d7b8f9100d9cfa79f5b58384c631260c137d9
Joseph Loutfi <joseph@anghami.com>
2021-11-10 19:31:24 +0400

    Section 5 of lecture: locators, get, find and contains commands, async promisses, saving elements into variables and chaining

--- cypress/integration/example/test1.spec.js
+++ cypress/integration/example/test1.spec.js
@@ -5,6 +5,39 @@ describe('My first test suite', function (){
         cy.wait(2000);
         cy.get('.product:visible').should('have.length', 4);
 
+        // check the number of products without using "visible"
+        // parent child chaining
+
+        cy.get('.products').find('.product').should('have.length', 4);
+
+        //eq(x) get the item at index x
+        cy.get('.products').find('.product').eq(1);
+
+        // while this can be reached using the cypress integrated tool from the browser, you should use the core concepts
+        //of automation to make the code more stable
+        // cy.get(':nth-child(3) > .product-action > button')
+        cy.get('.products').find('.product').eq(2).contains('ADD TO CART').click();
+
+        // #productLocator => aliasing
+        cy.get('.products').as('productLocator');
+        cy.get('@productLocator').find('.product').each((el, index, list) => {
+            let vegtext = el.find('.product-name').text();
+            if (vegtext.includes('Cash')) {
+                cy.wrap(el).find('button').click();
+            }
+        })
+
+        // Assigning found elements to variables => will have to handle the promise manually
+        cy.get('.brand').then((logoelement) => {
+            cy.log(logoelement.text());
+        })
+
+        cy.get('.brand').should('have.text', 'GREENKART');
+
+        // cy.get('.brand').text(): this will not work, text() is not cypress method or command
+        // this comes from JQuery: https://docs.cypress.io/api/table-of-contents
+        // When dealing with methods that are not part of cypress, then the you will have to resolve
+        // the promise manually using .then() since the commands do not resolve the promises by themselves
 
 
 
---
+++ cypress/integration/example/test2.spec.js
@@ -0,0 +1,21 @@
+describe('My 2nd test suite', function (){
+    it('My 2nd test case', function (){
+        cy.visit('https://rahulshettyacademy.com/seleniumPractise/#/');
+        cy.get('.search-keyword').type('ca');
+        cy.wait(2000);
+
+        cy.get('.products').as('productLocator');
+        cy.get('@productLocator').find('.product').each((el, index, list) => {
+            let vegtext = el.find('.product-name').text();
+            if (vegtext.includes('Cash')) {
+                cy.wrap(el).find('button').click();
+            }
+        })
+
+        cy.get('.cart-icon > img').click();
+        cy.get('button').contains('PROCEED TO CHECKOUT').click();
+        cy.wait(2000);
+        cy.get('button').contains('Place Order').click();
+
+    })
+})
