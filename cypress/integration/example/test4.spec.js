0f2b4bc528cbacd88dcd9de6172ac291a84cabf6
Joseph Loutfi <joseph@anghami.com>
2021-11-11 14:11:59 +0400

    testing popups and dealing with child windows

---
+++ cypress/integration/example/test4.spec.js
@@ -0,0 +1,30 @@
+describe('My 4rt test suite', () => {
+    it('tests popup', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+
+        //event: window:alert
+        cy.on('window:alert', (str) => {
+            // Will have to use mocha here instead of chai
+            expect(str).to.be.eq('Hello , share this practice page and share your knowledge');
+        })
+
+        cy.get('#alertbtn').click();
+
+        cy.on('window:confirm', (str) => {
+            // Will have to use mocha here instead of chai
+            expect(str).to.be.eq('Hello , Are you sure you want to confirm?');
+
+            // return true or false to represent "OK" or "Cancel"
+            return false
+        })
+
+        cy.get('#confirmbtn').click();
+    })
+
+    it('tests child tabs and navigate', () => {
+        cy.visit('https://rahulshettyacademy.com/AutomationPractice/');
+        cy.get('#opentab').invoke('removeAttr', 'target').click();
+        cy.url().should('include', 'cademy').and('not.include', 'Prac');
+        cy.go('back');
+    })
+})
